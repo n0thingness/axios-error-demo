f2a3f8ae57d3cbc53b36454b8b91a65ed0260d43
Daniel Briskin <daniel.briskin@gmail.com>
2023-01-16 17:20:43 +0900

    sendgrid error additions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import mail, { MailDataRequired } from "@sendgrid/mail";
 import { AxiosError } from "axios";
 
 class CustomError extends Error {
@@ -26,5 +27,39 @@ export const throwsCustomError = functions.https.onRequest((request, response) =
   throw new CustomError("This CustomError will be logged with severity 'Error'", "internal message");
 });
 
+// new functions
 
+const fakeApiKey = "fake-api-key";
 
+const mailData: MailDataRequired = {
+  to: "to@example.com",
+  from: "from@example.com",
+  replyTo: "from@example.com",
+  templateId: "fake-template",
+}
+
+export const mailThrowsErrorDirectly = functions.https.onRequest(async (request, response) => {
+  functions.logger.info("throwsResponseErrorDirectly called: will be logged with severity 'Error'");
+  mail.setApiKey(fakeApiKey);
+  await mail.send(mailData);
+});
+
+export const mailThrowsIncorrectlyWrappedError = functions.https.onRequest(async (request, response) => {
+  functions.logger.info("throwsBadlyWrappedResponseError called: will be logged with severity 'Default'");
+  mail.setApiKey(fakeApiKey);
+  try {
+    await mail.send(mailData);
+  } catch (err) {
+    throw new Error(`Caught Sendgrid error: ${err}`);
+  }
+});
+
+export const mailThrowsCorrectlyWrappedError = functions.https.onRequest(async (request, response) => {
+  functions.logger.info("throwsCorrectlyWrappedResponseError called: will be logged with severity 'Error'");
+  mail.setApiKey(fakeApiKey);
+  try {
+    await mail.send(mailData);
+  } catch (err) {
+    throw new Error("Caught Sendgrid error:", {cause: err} );
+  }
+});
